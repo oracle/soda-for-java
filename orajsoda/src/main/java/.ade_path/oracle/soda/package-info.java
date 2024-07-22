485d83b7ab27c4650821d79af666e9362f42345c
vedant.mahajan@oracle.com <vemahaja@phoenix137785.dev3sub3phx.databasede3phx.oraclevcn.com>
2024-07-22 23:17:34 +0000

    upload 4

--- orajsoda/src/main/java/.ade_path/oracle/soda/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * Contains the core SODA interfaces and classes.
- * <p>
- * These are designed to be implementable across various Oracle
- * data stores.
- * </p>
- */
-package oracle.soda;
--- orajsoda/src/main/java/oracle/soda/rdbms/.ade_path/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * Contains RDBMS-specific SODA interfaces and classes.
- * <p>
- * These are used by the Oracle RDBMS-based implementation.
- * </p>
- */
-package oracle.soda.rdbms;
