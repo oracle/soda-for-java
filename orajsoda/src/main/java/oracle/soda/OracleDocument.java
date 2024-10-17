3a45c03924e4969cd027e1021a290c936b8770af
Maxim Orgiyan <maxim.orgiyan@oracle.com>
2024-10-16 21:28:12 -0700

    Update OracleDocument.java

--- orajsoda/src/main/java/oracle/soda/OracleDocument.java
+++ orajsoda/src/main/java/oracle/soda/OracleDocument.java
@@ -114,9 +114,6 @@ public interface OracleDocument
    * </td>
    * <td style="border: 1px solid black; padding: 5px;">The JSON type value is returned as JSON text. </td>
    * </tr>
-   * <tr>
-   * <td style="border: 1px solid black; padding: 5px;">The JSON type value is returned as UTF8 JSON text. </td>
-   * </tr>
    * </table>
    * <b>Deprecation Notice:</b> support for {@code javax.json} will be removed in an upcoming release.
    *
